da5e50d5fd610565096319600e1e9e80b7e17427
Richie <mohammadpagard.dev@gmail.com>
2024-02-12 23:29:28 +0330

    Used .buck_create instead of .create method...

--- course/views.py
+++ course/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render
 from django.views import View
 from django.db.models import Q
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
